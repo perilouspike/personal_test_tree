c359ae015f6827e6ac2d8295a5a07af7f39451bc
Shibu Shaji <97983211+perilouspike@users.noreply.github.com>
2022-12-24 00:05:17 +0530

    Update postrecoveryboot.sh

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -8,14 +8,14 @@ mount -w /vendor > /dev/null 2>&1
 mount -w /odm > /dev/null 2>&1
 mount -w /product > /dev/null 2>&1
 mount -w /system_ext > /dev/null 2>&1
-mount -w /system_root > /dev/null 2>&1
+mount -w /system > /dev/null 2>&1
 
 sleep 1
 umount /vendor > /dev/null 2>&1
 umount /odm > /dev/null 2>&1
 umount /product > /dev/null 2>&1
 umount /system_ext > /dev/null 2>&1
-umount /system_root > /dev/null 2>&1
+umount /system > /dev/null 2>&1
 
 exit 0
 #
