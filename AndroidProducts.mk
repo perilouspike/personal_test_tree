f5972cbde58d43dccb86dbb5d02718551a7cbe19
perilouspike <97983211+perilouspike@users.noreply.github.com>
2026-05-24 21:27:32 +0530

    lunch fix. - fix: use three-part lunch combo format for Android 14 build system.

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,6 +17,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_CPH2579.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_CPH2579-trunk_staging-user \
-    twrp_CPH2579-trunk_staging-userdebug \
-    twrp_CPH2579-trunk_staging-eng    
+    twrp_CPH2579
