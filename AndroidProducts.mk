b1c751c3c8ef8be8b6b9b7937e461bdce7d8338b
perilouspike <97983211+perilouspike@users.noreply.github.com>
2026-05-25 21:57:22 +0530

    Refactor Android build configuration: update COMMON_LUNCH_CHOICES and adjust BoardConfig.mk settings

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,6 +17,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_CPH2579.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_CPH2579-eng \
+    twrp_CPH2579-user \
     twrp_CPH2579-userdebug \
-    twrp_CPH2579-user
+    twrp_CPH2579-eng
